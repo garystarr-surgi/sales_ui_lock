5af6f29b4dc58c7915f763e2f07bfe8cb185bf84
garystarr-surgi <gary.starr@surgishop.com>
2026-01-27 12:24:33 -0500

    Update disable_navbar_items.js

--- sales_ui_lock/public/js/disable_navbar_items.js
+++ sales_ui_lock/public/js/disable_navbar_items.js
@@ -1,8 +1,29 @@
-$(document).on('app_ready', function() {
-    // Remove 'User Settings' from the internal navbar settings list
-    if (frappe.boot.navbar_settings && frappe.boot.navbar_settings.settings_dropdown) {
-        frappe.boot.navbar_settings.settings_dropdown = frappe.boot.navbar_settings.settings_dropdown.filter(
-            item => item.item_label !== 'User Settings'
-        );
-    }
+// sales_ui_lock/public/js/global_ui_tweaks.js
+
+$(document).ready(function() {
+    const hideUserSettings = () => {
+        // Target by data-label and by href to be safe
+        $('li[data-label="User Settings"], a[href*="user-settings"]').hide();
+        $('li[data-label="My Settings"]').hide();
+    };
+
+    // Run immediately
+    hideUserSettings();
+
+    // Run whenever the menu is clicked (as v16 renders some items lazily)
+    $(document).on('click', '.navbar-user', function() {
+        setTimeout(hideUserSettings, 10);
+    });
+
+    // Global CSS injection as a final layer
+    $('<style>')
+        .prop('type', 'text/css')
+        .html(`
+            li[data-label="User Settings"], 
+            li[data-label="My Settings"], 
+            a[href="/app/user-settings"] { 
+                display: none !important; 
+            }
+        `)
+        .appendTo('head');
 });
